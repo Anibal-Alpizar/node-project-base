91de718fa9d6aa7bad98cab1453e1acec0af94f7
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-03 20:10:55 -0600

    first module

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const myModule = require("./module/myModule");
+const math = require('./math/index')
+// console.log(math)
 
-console.log(myModule)
+
+console.log(math.add(10,20))
+console.log(math.subtract(10,20))
+console.log(math.multiply(10,20))
+console.log(math.divide(10,20))
