ec71a0c0c83acec0967c917973d8982a43d132b5
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-05 15:23:58 -0600

    async/await

--- promise.js
+++ promise.js
@@ -11,8 +11,22 @@ const getText = (pathFile) => {
   });
 };
 
-getText("./data/fourth.txt")
-  .then((result) => console.log(result))
-  .then(() => getText("./data/first.txt"))
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+// getText("./data/fourth.txt")
+//   .then((result) => console.log(result))
+//   .then(() => getText("./data/first.txt"))
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+async function read() {
+    try {
+        // throw new Error('Error')
+        const result = await getText("./data/first.txt");
+        const result2 = await getText("./data/second.txt");
+        console.log(result);
+        console.log(result2);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+read();
