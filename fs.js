c3f46be70185cb9d3166f7a6a3a883cafd0468f9
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-04 20:11:02 -0600

    fs module(async)

--- fs.js
+++ fs.js
@@ -1,15 +1,31 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const first = fs.readFileSync('./data/first.txt', 'utf-8')
-const second = fs.readFileSync('./data/second.txt')
+// const first = fs.readFileSync('./data/first.txt', 'utf-8')
+// const second = fs.readFileSync('./data/second.txt')
 
-console.log(first)
-console.log(second.toString())
+// console.log(first)
+// console.log(second.toString())
 
+// const title = 'este es el contenido nuevo'
 
-const title = 'este es el contenido nuevo'
+// fs.writeFileSync('./data/fourth.txt', title,{
+//     flag: 'a'
+// })
 
+fs.readFile("./data/first.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+  fs.readFile("./data/second.txt", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(data);
 
-fs.writeFileSync('./data/fourth.txt', title,{
-    flag: 'a'
-})
+    fs.writeFile("./data/newFile.txt", "creado desde fs", (error, data) => {
+      console.log(error);
+      console.log(data);
+    });
+  });
+});
