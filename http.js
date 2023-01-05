f9c418218698a65ebafc27f8cde86711e9861c85
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-04 22:56:19 -0600

    http requests

--- http.js
+++ http.js
@@ -1,9 +1,24 @@
 const http = require("http");
 
-http.createServer((request, response) => {
-  response.write("Hello world");
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.write("welcome to the server");
+    return response.end();
+  }
+
+  if (request.url === "/about") {
+    response.write("acarca de");
+    return response.end();
+  }
+
+  response.write(`
+    <h1>Not Found</h1>
+    <p>This page not found</p>
+    <a href='/'>Volver a la pagina principal</a>
+    `);
   response.end();
-}).listen(3000);
+});
 
-console.log('server is running on port 3000')
+server.listen(3000);
 
+console.log("server is running on port 3000");
